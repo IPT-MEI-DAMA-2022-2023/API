c98f1b434dbc6bada342c5fb494193b666a5f488
jcnpereira <casimiro@ipt.pt>
2023-06-14 19:35:04 +0100

    files to express the interface used by user (res files)

--- app/src/main/java/pt/ipt/dama/api/ui/activity/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama/api/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package pt.ipt.dama.api
+package pt.ipt.dama.api.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pt.ipt.dama.api.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
